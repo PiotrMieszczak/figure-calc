c4a1891d98c01c421df9995744f7975001245ceb
PiotrMieszczak <mieszczak.piotr@gmail.com>
2019-06-15 11:10:19 +0200

    feat(app): components set-up
    components set-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {CustomMaterialModule} from './material.module';
+import { CustomMaterialModule } from './material.module';
+import { StepperComponent } from './container/stepper/stepper.component';
+import { ChoiceFormComponent } from './components/choice-form/choice-form.component';
+import { ResultsFormComponent } from './components/results-form/results-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StepperComponent,
+    ChoiceFormComponent,
+    ResultsFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/choice-form/choice-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choice-form',
+  templateUrl: './choice-form.component.html',
+  styleUrls: ['./choice-form.component.scss']
+})
+export class ChoiceFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
